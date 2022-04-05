28c593a40a3719edadd7d4deed8f918bb74a8201
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2022-04-05 09:37:38 -0500

    Test button click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('red');
+  const newColor =color === 'red' ? 'blue' : 'red';
+
   return (
     <div>
-      <button style={{backgroundColor: 'red'}}>
-        Change to blue
+      <button style={{backgroundColor: color}}
+      onClick={()=>setColor(newColor)}>
+        Change to {newColor}
       </button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
   render(<App />);
   const button = screen.getByRole('button',{name: 'Change to blue'});
+  // expect the background color to be red
   expect (button).toHaveStyle({backgroundColor : 'red'});
+  // click button
+  fireEvent.click(button);
+  // expect the background color to be blue
+  expect (button).toHaveStyle({backgroundColor : 'blue'});
+  //expect the button to have the text "Change to red"
+  expect(button.textContent).toBe('Change to red');
+
 });
